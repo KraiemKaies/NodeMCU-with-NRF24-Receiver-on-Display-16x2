0336f8f1900dffde448e77a797bbab929fecc09b
Kaies Kraiem <kraiem.kaies@gmail.com>
2022-08-16 11:28:50 +0100

    Add LCD Display Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,47 @@
 #include <Arduino.h>
-
 #include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-
-
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x3F, 16, 2);
 RF24 radio(2, 4); // CE, CSN
 const byte address[6] = "00001";
 boolean button_state = 0;
 uint8_t led_pin = D0;
-void setup() {
-pinMode(6, OUTPUT);
-Serial.begin(115200);
-radio.begin();
-radio.openReadingPipe(0, address);   //Setting the address at which we will receive the data
-radio.setPALevel(RF24_PA_MIN);       //You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
-radio.startListening();              //This sets the module as receiver
+void setup()
+{
+  lcd.init();
+  lcd.backlight();
+  lcd.clear();
+  lcd.setCursor(1, 0);
+  lcd.print("Welcome");
+  pinMode(6, OUTPUT);
+  Serial.begin(115200);
+  radio.begin();
+  radio.openReadingPipe(0, address); // Setting the address at which we will receive the data
+  radio.setPALevel(RF24_PA_MIN);     // You can set this as minimum or maximum depending on the distance between the transmitter and receiver.
+  radio.startListening();            // This sets the module as receiver
 }
 void loop()
 {
-if (radio.available())              //Looking for the data.
-{
-char text[32] = "";                 //Saving the incoming data
-radio.read(&text, sizeof(text));    //Reading the data
-radio.read(&button_state, sizeof(button_state));    //Reading the data
-if(button_state == HIGH)
-{
-digitalWrite(6, HIGH);
-Serial.println(text);
-}
-else
-{
-digitalWrite(6, LOW);
-Serial.println(text);}
-}
-delay(50);
+  if (radio.available()) // Looking for the data.
+  {
+    char text[32] = "";                              // Saving the incoming data
+    radio.read(&text, sizeof(text));                 // Reading the data
+    radio.read(&button_state, sizeof(button_state)); // Reading the data
+    if (button_state == HIGH)
+    {
+      digitalWrite(6, HIGH);
+      Serial.println(text);
+      lcd.print(text);
+    }
+    else
+    {
+      digitalWrite(6, LOW);
+      Serial.println(text);
+      lcd.print(text);
+    }
+  }
+  delay(50);
+  lcd.clear();
 }
